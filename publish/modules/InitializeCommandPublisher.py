d84dc37117995328fc9def232c96d6433184840d
tomoya-y <yamanokuchi.tomoya.ys9@is.naist.jp>
2025-04-07 19:31:21 +0900

    add valve init position

--- publish/modules/InitializeCommandPublisher.py
+++ publish/modules/InitializeCommandPublisher.py
@@ -18,14 +18,13 @@ class InitializeCommandPublisher:
             queue_size = self.queue_size,
         )
         # -------
-        self._num_ctrl            = 9
-        self._num_current_limit   = 9
-        self._num_position_p_gain = 9
-        self._num_init_command    = (self._num_ctrl + self._num_current_limit + self._num_position_p_gain)
+        self._num_joint_ctrl     = 9
+        self._num_valve_position = 1
+        self._num_command = (self._num_joint_ctrl + self._num_valve_position)
 
 
     def publish(self, initialize_command: np.ndarray):
-        assert initialize_command.shape == (self._num_init_command,)
+        assert initialize_command.shape == (self._num_command,)
         # ---
         self._msg.data = tuple(initialize_command)
         self._pub.publish(self._msg)
--- publish/publisher.py
+++ publish/publisher.py
@@ -17,11 +17,17 @@ class Publisher:
         self.valve_ctrl      = ValveCtrlPublisher(params)
 
 
-    def publish_initialize_ctrl(self, ctrl: Orientation):
-        assert isinstance(ctrl, Orientation)
-        initialize_command = np.hstack([ctrl.as_resvec(), self.current_limit, self.position_p_gain])
+    def publish_initialize_ctrl(self,
+            robot_ctrl: Orientation,
+            valve_ctrl: Orientation,
+        ):
+        assert isinstance(robot_ctrl, Orientation)
+        assert isinstance(valve_ctrl, Orientation)
+        # ----
+        initialize_command = np.hstack([robot_ctrl.as_resvec(), valve_ctrl.as_resvec()])
         self.initialize_ctrl.publish(initialize_command)
-        self.joint_ctrl.publish(ctrl.as_resvec()) # <-- 必要：無いと初期化前の直前に使用された制御入力が入力され続けてしまう
+        # ----
+        self.joint_ctrl.publish(robot_ctrl.as_resvec()) # <-- 必要：無いと初期化前の直前に使用された制御入力が入力され続けてしまう
 
     def publish_joint_ctrl(self, ctrl: Orientation):
         self.joint_ctrl.publish(ctrl.as_resvec())
